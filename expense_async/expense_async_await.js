d8a2e3466a8453a75ab2980a7fcaf7c70f1d03f5
rampion19 <princegupta.pg19@gmail.com>
2023-01-24 11:51:55 +0530

    final expense tracer project by async and await

--- expense_async/expense_async_await.js
+++ expense_async/expense_async_await.js
@@ -1,6 +1,5 @@
-
 const form = document.getElementById("form");
-
+const url = `https://crudcrud.com/api/1e9c400054494ec1b8a7dfb88260ac5d/expense`;
 form.addEventListener("submit",async function (e){
     e.preventDefault();
     const amt = e.target.amount.value;
@@ -13,7 +12,7 @@ form.addEventListener("submit",async function (e){
         ctg
     }
     try{
-        const response = await axios.post(`https://crudcrud.com/api/797bf733ce8e4824956412a0a60724e9/expense`, obj);
+        const response = await axios.post(url, obj);
         update(response.data);
         console.log(response.data);
     }
@@ -21,14 +20,54 @@ form.addEventListener("submit",async function (e){
     console.log(err)}
 })
 
-function update(obj){
-    document.getElementById("amount").value = "";
-    document.getElementById("description").value = "";
-    document.getElementById("catagory").value = "";
-
-    const li = `<li class="list-group-item border-success mt-3" id="${obj._id}">${obj.amt}, ${obj.desc}, ${obj.ctg}<button class="btn btn-danger btn-sm" onClick="deleted('${obj._id}')">Delete</button>
+function update(obj) {
+    const li = `<li class="list-group-item border-success mt-3" id="${obj._id}">${obj.amt}, ${obj.desc}, ${obj.ctg}<button class="btn btn-danger btn-sm" onClick="deleted('${obj._id}')">delete</button>
     <button class="btn btn-warning btn-sm" onClick="edit('${obj._id}','${obj.amt}','${obj.desc}','${obj.ctg}')">edit</button></li>`
-
-    var target = document.getElementById("ulList");
-    target.innerHTML +=li;
+    var target = document.getElementById("ullist");
+    target.innerHTML += li;
 }
+
+function deleteFromScreen(userid) {
+    var parent = document.getElementById("ullist")
+    var child = document.getElementById(userid);
+    parent.removeChild(child);
+}
+
+async function deleted(userid) {
+    try{
+        const response = await axios.delete(`${url}/${userid}`)
+        deleteFromScreen(userid)
+    }catch(err){
+        console.log(err)
+        }
+}
+
+async function edit(userid, amount, description, catagory) {
+    try{
+        document.getElementById('amount').value = amount;
+        document.getElementById('description').value = description;
+        document.getElementById('catagory').value = catagory;
+        deleted(userid)
+        const response = await axios.put(`${url}`/userid, {
+            a: document.getElementById("amount").value,
+            d: document.getElementById("description").value,
+            c: document.getElementById("catagory").value
+        })
+        
+        console.log(response.data);
+    }catch(err){
+        // console.log(err)
+    }
+}
+
+window.addEventListener("DOMContentLoaded", async() => {
+    try{
+        const response = await axios.get(url)
+            for (var i = 0; i < response.data.length; i++) {
+                update(response.data[i]);
+            }
+    }catch(err){
+        console.log(err)
+        }
+
+})
